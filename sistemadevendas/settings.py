79f070f1add2270f51b3e607f0c0ed5b9c80857d
Jonathan <jonathan.mendes@buildit.com.br>
2024-11-16 15:50:58 -0300

    alterando host permitido

--- sistemadevendas/settings.py
+++ sistemadevendas/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-(@$@85@x8su+5f14gkn9a3ijbm&#%_03wyga%*c+#^ywzj=0&b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://sistemakoreanchicken.onrender.com/']
 
 
 # Application definition
